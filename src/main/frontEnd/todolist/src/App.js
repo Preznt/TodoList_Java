9454b48c47a072a8637988dee7dd45d5925de704
Preznt <bjw1403@gmail.com>
2023-07-22 22:27:31 +0900

    react-slick을 이용해서 캐러셀 구현 및 dots 커스텀

--- src/main/frontEnd/todolist/src/App.js
+++ src/main/frontEnd/todolist/src/App.js
@@ -1,18 +1,33 @@
 import { useState } from "react";
 import "./css/App.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./css/slider-custom.css";
 import Login from "./comps/Login";
 import Section from "./comps/Section";
 import Title from "./comps/Title";
+import Slider from "react-slick";
 import CalendarContainer from "./comps/calendar/CalendarContainer";
 
 function App() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dotsClass: "dots_custom",
+  };
+
   return (
     <div className="App">
-      <CalendarContainer />
-      {/* <div className="flex flex-col place-items-center my-9">
-        <Title />
-        <Section />
-      </div> */}
+      <Slider {...settings}>
+        <div className="flex flex-col place-items-center my-9">
+          <Title />
+          <Section />
+        </div>
+        <CalendarContainer />
+      </Slider>
       {/* <Login /> */}
     </div>
   );
